fdb09ac2653de9d7dbf0f6a87a70d7e7ed30db41
Renner01 <david.renner.dst@gmail.com>
2022-09-19 21:23:45 +0200

    javítottam a hibát

--- main.cpp
+++ main.cpp
@@ -4,32 +4,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    //Elírás."N_ELEMENTS" kellene ide.
-    int *b = new int[NELEMENTS];
-    //Nincs sorlezárás.
-    std::cout << '1-100 ertekek duplazasa'
+    //Elírás. "N_ELEMENTS" kellene ide.
+    int *b = new int[N_ELEMENTS];
+    //Nincs sorlezárás
+    std::cout << '1-100 ertekek duplazasa' << std::endl;
     //Hiányos for ciklus.
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //0-tól 99-ig duplázunk és nem 1-től 100-ig
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
     //Ebbe a ciklusba nem lépünk be.
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //Az értéket ki nem írjuk ki, és nincs sorlezárás sem.
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //Nincs kezdőértéke a változónak.
-    int atlag;
+    int atlag = 0;
     //Pontosvessző helyett sima vessző található a lépésköz előtt.
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //Hiányzó sorlezárás.
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
 }
